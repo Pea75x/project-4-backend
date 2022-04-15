7feaa9504617d179f6f400d5512f8dc9f46f2298
Priya <priya_93_x@hotmail.co.uk>
2022-04-15 08:00:04 +0100

    fix bug

--- festivals/models.py
+++ festivals/models.py
@@ -3,17 +3,17 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Festival(models.Model):
-  ALL = 'All'
-  CROATIA = 'Croatia'
-  MALTA = 'Malta'
+  # ALL = 'All'
+  # CROATIA = 'Croatia'
+  # MALTA = 'Malta'
 
-  LOCATION_CHOICES = [
-        (ALL, 'All'),
-        (CROATIA, 'Croatia'),
-        (MALTA, 'Malta'),
-    ]
+  # LOCATION_CHOICES = [
+  #       (ALL, 'All'),
+  #       (CROATIA, 'Croatia'),
+  #       (MALTA, 'Malta'),
+  #   ]
   name = models.CharField(max_length=50)
-  location = models.CharField(max_length=20, choices=LOCATION_CHOICES, default=ALL)
+  #location = models.CharField(max_length=20, choices=LOCATION_CHOICES, default=ALL)
   start_date = models.DateField(null=True)
   end_date = models.DateField(null=True)
   image = models.CharField(max_length=200, null=True)
